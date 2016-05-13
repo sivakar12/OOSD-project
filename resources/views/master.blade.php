99f449a050068207bec24e00fd245c241d274360
Sivakar <sivakar12@outlook.com>
2016-05-13 08:45:06 +0530

    Create master layout

---
+++ resources/views/master.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Bitulink Dealings</title>
+		<link rel="stylesheet" href="/bootstrap-darkly.min.css">
+		<script src="jquery.min.js"></script>
+		<script src="bootstrap.min.js"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nb">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span> 
+      				</button>
+					<a class="navbar-brand" href="/">Bitulink Dealings</a>
+				</div>
+				<div class="collapse navbar-collapse" id="nb">
+					<ul class="nav navbar-nav">
+						<li><a href="/suppliers">Suppliers</a></li>
+						<li><a href="#">Customers</a></li>
+						<li><a href="/pi">Performa Invoice</a></li>
+						<li><a href="#">Purchase Order</a></li>
+						<li><a href="/sales_invoices">Sales Inovices</a></li>
+						<li><a href="#">Stock</a></li>
+						<li><a href="/users">Users</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						@if(Auth::check())
+						<li><a href="/logout">Logout ({{ Auth::user()->username }})</a></li>
+						@else
+						<li><a href="/login">Login</a></li>
+						@endif
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="container">
+			@yield('content')
+		</div>
+	</body>
+</html>
