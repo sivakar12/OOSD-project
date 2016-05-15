b34bdb085f0bce7bade2c4f1eaaaae22dc2210d2
Sivakar <sivakar12@outlook.com>
2016-05-15 11:13:37 +0530

    Create model and migrations for voucher

---
+++ database/migrations/2016_05_15_050645_create_vouchers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVouchersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('vendor');
+            $table->string('descripton');
+            $table->decimal('amount', 12, 2);
+            $table->string('payment_type');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('vouchers');
+    }
+}
