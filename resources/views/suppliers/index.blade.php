56cc0121175243ecb2063cab0759d6118d6ca11a
Sivakar <sivakar12@outlook.com>
2016-05-17 18:50:25 +0530

    Improved Supplier search

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="col-md-offset-3 col-md-6">
 		<h1>Suppliers List</h1>
-		<a class="btn btn-default pull-right" href="/suppliers/new">New</a>
-		<input type="text" id="searchbox" class="form-control" placeholder="Search">
-		<br><br>
+		<a class="btn btn-default pull-right" href="/suppliers/new">New</a><br><br><br>
+		<input type="text" id="searchbox" class="form-control" placeholder="Search"><br>
+		
 		<ul class="list-group">
 		@foreach($suppliers as $supplier)
-			<li class="list-group-item">
-				{{ $supplier->name }}
+			<li class="list-group-item supplier-view">
+				<span class="supplier-name">{{ $supplier->name }}</span>
 				<a class="pull-right" href="/suppliers/{{$supplier->id}}">Details</a>
 			</li>
 		@endforeach
@@ -20,11 +20,11 @@ $(document).ready(function(){
 	$('#searchbox').keyup(function() {
 		var filter = $(this).val();
 
-		$('.list-group-item').each(function() {
+		$('.supplier-name').each(function() {
 			if ($(this).text().search(new RegExp(filter, 'i')) < 0) {
-				$(this).fadeOut();
+				$(this).parent().fadeOut();
 			} else {
-				$(this).show();
+				$(this).parent().fadeIn();
 			}
 		});
 	});
