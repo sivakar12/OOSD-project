a4d1225accdd7ab65e3cf42b32289bf23ec7a38f
Sivakar <sivakar12@outlook.com>
2016-05-17 10:34:31 +0530

    Add search for supplier

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -4,6 +4,7 @@
 <div class="col-md-offset-3 col-md-6">
 		<h1>Suppliers List</h1>
 		<a class="btn btn-default pull-right" href="/suppliers/new">New</a>
+		<input type="text" id="searchbox" class="form-control" placeholder="Search">
 		<br><br>
 		<ul class="list-group">
 		@foreach($suppliers as $supplier)
@@ -14,4 +15,19 @@
 		@endforeach
 		</ul>
 </div>
+<script>
+$(document).ready(function(){
+	$('#searchbox').keyup(function() {
+		var filter = $(this).val();
+
+		$('.list-group-item').each(function() {
+			if ($(this).text().search(new RegExp(filter, 'i')) < 0) {
+				$(this).fadeOut();
+			} else {
+				$(this).show();
+			}
+		});
+	});
+});
+</script>
 @endsection
