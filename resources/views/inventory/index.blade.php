ced3e34d3e51777c3bf73886107e52ea12bbbd05
Sivakar <sivakar12@outlook.com>
2016-05-18 12:43:01 +0530

    Add search for inventory

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -5,6 +5,14 @@
 		<h1>Inventory</h1>
 		<a class="btn btn-default pull-right" href="/inventory/new">Add Vehicle</a>
 		<br><br>
+		<div class="row">
+			<div class="col-md-6">
+			<input type="text" id="cn-search" class="form-control" placeholder="Find by Chassis Number">
+			</div>
+			<div class="col-md-6">
+			<input type="text" id="model-search" class="form-control" placeholder="Search for Model">
+			</div>
+		</div>
 		<table class="table table-striped">
 			<thead>
 				<tr>
@@ -16,8 +24,8 @@
 			<tbody>
 			@foreach($items as $item)
 				<tr>
-					<td>{{ $item->chassis_number }}</td>
-					<td>{{ $item->manufacturer}} {{ $item->model }}</td>
+					<td class="cn">{{ $item->chassis_number }}</td>
+					<td class="model">{{ $item->manufacturer}} {{ $item->model }}</td>
 					<td>{{ $item->sales_price }}</td>
 					<td><a href="/inventory/{{$item->id}}">Details</a></td>
 				</tr>
@@ -25,4 +33,30 @@
 			</tbody>
 		</table>
 </div>
+<script>
+$(document).ready(function(){
+	$('#cn-search').keyup(function() {
+		var filter = $(this).val();
+
+		$('.cn').each(function() {
+			if ($(this).text().search(new RegExp(filter, 'i')) < 0) {
+				$(this).parent().fadeOut();
+			} else {
+				$(this).parent().fadeIn();
+			}
+		});
+	});
+	$('#model-search').keyup(function() {
+		var filter = $(this).val();
+
+		$('.model').each(function() {
+			if ($(this).text().search(new RegExp(filter, 'i')) < 0) {
+				$(this).parent().fadeOut();
+			} else {
+				$(this).parent().fadeIn();
+			}
+		});
+	});
+});
+</script>
 @endsection
